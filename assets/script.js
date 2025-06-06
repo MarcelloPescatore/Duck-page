04dc66b189ed4f9d4a1aa6a8a6b40618cec17b72
MarcelloPescatore <pescatoremarcello@gmail.com>
2025-06-07 00:21:18 +0200

    add more content and style it: - feedback with slider - collection

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,20 @@ const openButton = document.getElementById('open-menu');
 const closeButton = document.getElementById('close-menu');
 
 
-openButton.addEventListener('click', function(){
+openButton.addEventListener('click', function () {
     sidebar.style.display = 'flex';
     overlay.style.display = 'inline';
 });
 
-closeButton.addEventListener('click', function(){
+closeButton.addEventListener('click', function () {
     sidebar.style.display = 'none';
     overlay.style.display = 'none';
+});
+
+var swiper = new Swiper(".mySwiper", {
+    direction: 'horizontal',
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
 });
